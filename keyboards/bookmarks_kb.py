4d88371960648c791b66a8b9e8bd0f626b8b29f5
Daniil Karkozov <dkarkozov@gmail.com>
2023-05-11 16:56:55 +0300

    book_func

---
+++ keyboards/bookmarks_kb.py
@@ -0,0 +1,6 @@
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from services.file_handling import book
+from lexicon.lexicon import LEXICON
+
+print(book)
